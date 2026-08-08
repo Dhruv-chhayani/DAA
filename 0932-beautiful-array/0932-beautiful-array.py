3da584a884d91c65b00807cffb954613b00a3f90
Dhruv-chhayani <155423011+Dhruv-chhayani@users.noreply.github.com>
2026-08-08 13:58:27 +0530

    Time: 4 ms (17.75%), Space: 19.3 MB (46.05%) - LeetHub

---
+++ 0932-beautiful-array/0932-beautiful-array.py
@@ -0,0 +1,17 @@
+class Solution:
+    def beautifulArray(self, n: int) -> List[int]:
+        if n == 1:
+            return [1]
+
+        even = self.beautifulArray(n // 2)
+        odd = self.beautifulArray((n + 1) // 2)
+
+        result = []
+
+        for x in odd:
+            result.append(2 * x - 1)
+
+        for x in even:
+            result.append(2 * x)
+
+        return result
